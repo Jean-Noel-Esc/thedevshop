6305c2429f41f858036855b66b9b4366b26ad27f
Jean-Noel-Esc <60701865+Jean-Noel-Esc@users.noreply.github.com>
2021-07-19 11:46:15 +0200

    creation navbar
    + creation styles.js in Products to adjust the display of product card

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
+
 
 const products = [
     { id:1, name: "shoes", description: "Running shoes.", price: "$5" },
@@ -9,8 +11,12 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
+
+
     return ( 
-        <main>
+        <main classname={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 { products.map((product) => (
                     <Grid item key= {product.id} xs={12} sm={6} md={4} lg={3}>
